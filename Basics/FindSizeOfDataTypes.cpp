d11b71cadfb47a53fd38b58703280b99eb04f381
Pratik Borse <93337756+Zepar99@users.noreply.github.com>
2025-08-19 19:05:16 +0530

    Finding the Size Of Data Types

---
+++ Basics/FindSizeOfDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+int main()
+{
+    char charsize;
+    bool boolsize;
+    int intsize;
+    float floatsize;
+    double doublesize;
+       
+    std:: cout << "Size of char is: " << sizeof(charsize) << "\n";
+    std:: cout << "Size of bool is: " << sizeof(boolsize)<< "\n";
+    std:: cout << "Size of Int is: " << sizeof(intsize)<< "\n";
+    std:: cout << "Size of float is: " << sizeof(floatsize)<< "\n";
+    std:: cout << "Size of double is: " << sizeof(doublesize)<< "\n";
+ 
+}
