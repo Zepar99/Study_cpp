82224fdaf4278c3e97e01b41cd6a9fd1df7f4e53
Pratik Borse <93337756+Zepar99@users.noreply.github.com>
2025-08-19 18:45:11 +0530

    AddTwoNumbers

---
+++ Basics/AddTwoNumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main()
+{
+ int x;
+ int y;
+ int sum;
+ 
+ std:: cout << "Enter your first Number: \n";
+ std::cin >> x; 
+ 
+ std:: cout << "Enter Your second Number: \n";
+ std::cin >> y;
+ sum = x+y;
+ 
+ std :: cout << "Sum is " << sum;
+ 
+}
